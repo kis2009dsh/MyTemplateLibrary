b7ef4dd4122f01a512dc919841adcab7ea84fcc5
kis2009dsh <kis2009dsh@vip.qq.com>
2013-11-07 17:24:01 +0800

    Initial Commit

---
+++ MTL/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MTL
+//
+//  Created by dongshihao on 13-11-7.
+//  Copyright (c) 2013年 dongshihao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
